ded29ec15d59dd3a83f0fc4093e48d3092808c1b
kavipriya2912002 <kavipriya2912002@gmail.com>
2024-12-16 12:42:03 +0530

    login form changed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,9 +13,9 @@ function Login() {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-slate-200">
-        <img src={background} alt="back" />
+        
       <div className="w-full max-w-md p-8 space-y-6 bg-white shadow-md rounded-lg">
-        <h2 className="text-2xl font-bold text-center">Login</h2>
+        <h2 className="text-2xl font-bold text-center">Let's Login</h2>
         <form onSubmit={handleLogin} className="space-y-6">
           <div>
             <label className="block text-left text-gray-700">Email</label>
