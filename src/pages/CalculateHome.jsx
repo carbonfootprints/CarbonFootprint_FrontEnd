294db17988c2c93ea3d2ab275fc56b2a6d7b6f5b
kavipriya2912002 <kavipriya2912002@gmail.com>
2024-12-17 18:41:45 +0530

    direct ghg added

--- src/pages/CalculateHome.jsx
+++ src/pages/CalculateHome.jsx
@@ -1,4 +1,4 @@
-import  { useState } from 'react';
+import { useState } from 'react';
 import Organisation from '../components/Organisation'; // Adjust the path as per your project structure
 import ProductionSystem from '../components/ProductionSystem';
 import Boundary from '../components/Boundary';
@@ -19,49 +19,44 @@ function CalculateHome() {
       <div className="grid grid-cols-6 gap-2 mb-6">
         <button
           onClick={() => handleTabChange('organisation')}
-          className={`col-span-6 sm:col-span-1 font-bold py-3 rounded-lg transition ${
-            activeTab === 'organisation' ? 'bg-green-600 text-white' : 'bg-green-200 text-green-800'
-          }`}
+          className={`col-span-6 sm:col-span-1 font-bold py-3 rounded-lg transition ${activeTab === 'organisation' ? 'bg-green-600 text-white' : 'bg-green-200 text-green-800'
+            }`}
         >
           Organisation
         </button>
         <button
           onClick={() => handleTabChange('productionSystem')}
-          className={`col-span-6 sm:col-span-1 font-bold py-3 rounded-lg transition ${
-            activeTab === 'productionSystem' ? 'bg-green-600 text-white' : 'bg-green-200 text-green-800'
-          }`}
+          className={`col-span-6 sm:col-span-1 font-bold py-3 rounded-lg transition ${activeTab === 'productionSystem' ? 'bg-green-600 text-white' : 'bg-green-200 text-green-800'
+            }`}
         >
           Production System
         </button>
         <button
           onClick={() => handleTabChange('boundary')}
-          className={`col-span-6 sm:col-span-1 font-bold py-3 rounded-lg transition ${
-            activeTab === 'boundary' ? 'bg-green-600 text-white' : 'bg-green-200 text-green-800'
-          }`}
+          className={`col-span-6 sm:col-span-1 font-bold py-3 rounded-lg transition ${activeTab === 'boundary' ? 'bg-green-600 text-white' : 'bg-green-200 text-green-800'
+            }`}
         >
           Boundary
         </button>
         <button
           onClick={() => handleTabChange('directGHG')}
-          className={`col-span-6 sm:col-span-1 font-bold py-3 rounded-lg transition ${
-            activeTab === 'directGHG' ? 'bg-green-600 text-white' : 'bg-green-200 text-green-800'
-          }`}
+          className={`col-span-6 sm:col-span-1 font-bold py-3 rounded-lg transition ${activeTab === 'directGHG' ? 'bg-green-600 text-white' : 'bg-green-200 text-green-800'
+            }`}
         >
           Direct GHG
         </button>
         <button
           onClick={() => handleTabChange('inDirectGHG')}
-          className={`col-span-p6 sm:col-span-1 font-bold py-3 rounded-lg transition ${
-            activeTab === 'inDirectGHG' ? 'bg-green-600 text-white' : 'bg-green-200 text-green-800'
-          }`}
+          className={`col-span-6 sm:col-span-1 font-bold py-3 rounded-lg transition ${activeTab === 'inDirectGHG' ? 'bg-green-600 text-white' : 'bg-green-200 text-green-800'
+            }`}
         >
           InDirect GHG
         </button>
+
         <button
           onClick={() => handleTabChange('getReport')}
-          className={`col-span-6 sm:col-span-1 font-bold py-3 rounded-lg transition ${
-            activeTab === 'getReport' ? 'bg-green-600 text-white' : 'bg-green-200 text-green-800'
-          }`}
+          className={`col-span-6 sm:col-span-1 font-bold py-3 rounded-lg transition ${activeTab === 'getReport' ? 'bg-green-600 text-white' : 'bg-green-200 text-green-800'
+            }`}
         >
           Get Report
         </button>
@@ -70,11 +65,11 @@ function CalculateHome() {
       {/* Content */}
       <div className="p-4 border rounded-lg bg-gray-100 shadow-lg">
         {activeTab === 'organisation' && <Organisation />}
-        {activeTab === 'productionSystem' && <ProductionSystem/>}
-        {activeTab === 'boundary' && <Boundary/>}
-        {activeTab === 'directGHG' && <DirectGHG/>}
-        {activeTab === 'inDirectGHG' && <IndirectGHG/>}
-        {activeTab === 'getReport' && <Report/>}
+        {activeTab === 'productionSystem' && <ProductionSystem />}
+        {activeTab === 'boundary' && <Boundary />}
+        {activeTab === 'directGHG' && <DirectGHG />}
+        {activeTab === 'inDirectGHG' && <IndirectGHG />}
+        {activeTab === 'getReport' && <Report />}
       </div>
     </div>
   );
