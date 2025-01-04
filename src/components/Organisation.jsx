4f4b955862fb321aca9f077f1a138b239f633f95
kavipriya2912002 <kavipriya2912002@gmail.com>
2025-01-04 12:55:43 +0530

    toast notification added

--- src/components/Organisation.jsx
+++ src/components/Organisation.jsx
@@ -31,6 +31,7 @@ function Organisation() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true); // Set loading to true
+console.log(formData);
 
     try {
       const response = await fetch(`${BACKEND_URL}/api/direct/organisation`, {
